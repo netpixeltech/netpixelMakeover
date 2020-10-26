cc6ebd082a0996fc0fb3645bafb222702ddbddff
Jai Mata Di <jaimatadi@Jais-iMac.local>
2020-10-26 19:02:41 +0530

    removed graphql

--- src/components/MyParallax.js
+++ src/components/MyParallax.js
@@ -7,36 +7,36 @@ import reception from "../images/reception.jpg"
 import ParallaxImg from './ParallaxImg';
 import BottomImgs from './ParallaxBootom'
 
-import {graphql,useStaticQuery} from "gatsby"
+// import {graphql,useStaticQuery} from "gatsby"
 
 function MyParallax() {
 
-    const data=useStaticQuery(graphql`
-    query{
-        allSanityImagePost{
-            edges{
-              node{
-               title
-                mainImage{
-                  asset{
-                    url
-                  }
-                }
-              }
-            }
-          }
-        }
-    `)
+    // const data=useStaticQuery(graphql`
+    // query{
+    //     allSanityImagePost{
+    //         edges{
+    //           node{
+    //            title
+    //             mainImage{
+    //               asset{
+    //                 url
+    //               }
+    //             }
+    //           }
+    //         }
+    //       }
+    //     }
+    // `)
 
     //   let resource = data.allSanityPost.edges[2].node;
     return (
         <div>
-             {
+             {/* {
                 data.allSanityImagePost.edges.map((post,index)=>{
                   return <ParallaxImg key={index} imgtitle={post.node.title} img={post.node.mainImage.asset.url} />
                 })
             }
-           
+            */}
              
             <ParallaxImg imgtitle={"Bridal"} img={bridal} />
             <ParallaxImg imgtitle={"Cocktail"} img={cocktail} />
