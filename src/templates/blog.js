0023428c49e776c926b0d70dc5239b945f055f80
Jai Mata Di <jaimatadi@Jais-iMac.local>
2020-10-28 15:13:58 +0530

    added text to blog

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import {graphql} from 'gatsby'
-
+import BasePortableText from '@sanity/block-content-to-react'
 export const query = graphql`
 query(
     $title:String!
@@ -12,22 +12,35 @@ query(
     }
   ){
       title
+      mainImage{
+        asset{
+          url
+        }
+      }
     publishedAt
     slug{
       current
     }
-    
+   
+    _rawBody
 
-    mainImage{
-        asset{
-         url
-       } 
-       }
+   
   }
   }
 `
 function Blog(props) {
 
+  const serializers = {
+    types: {
+      code: props => (
+        <pre data-language={props.node.language}>
+          <code>{props.node.code}</code>
+        </pre>
+      ),
+      mainImage: "Figure",
+      authorReference: ({node}) => <span>{node.author.name}</span>
+    }
+  }
 
   
 
@@ -44,9 +57,35 @@ function Blog(props) {
            <p className="lead">
                {props.data.sanityPost.slug.current}
            </p>
-          
+           <BasePortableText blocks={props.data.sanityPost._rawBody} serializers={serializers} />
         </Layout>
     )
 }
 
 export default Blog
+
+// const React = require('react')
+// const ReactDOM = require('react-dom')
+// const BlockContent = require('@sanity/block-content-to-react')
+// const client = require('@sanity/client')({
+//   projectId: `zkyvt2x6`,
+//   dataset: `production`,
+//   useCdn: true
+// })
+ 
+// const serializers = {
+//   types: {
+//     code: props => (
+//       <pre data-language={props.node.language}>
+//         <code>{props.node.code}</code>
+//       </pre>
+//     )
+//   }
+// }
+ 
+// client.fetch('*[_type == "article"][0]').then(article => {
+//   ReactDOM.render(
+//     <BlockContent blocks={article.body} serializers={serializers} />,
+//     document.getElementById('root')
+//   )
+// })
