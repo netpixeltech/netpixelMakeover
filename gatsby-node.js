c660fa8397279c9517db5802dec38d19dbc01fe6
Jai Mata Di <jaimatadi@Jais-iMac.local>
2020-10-29 16:31:27 +0530

    fixing issues

--- gatsby-node.js
+++ gatsby-node.js
@@ -28,3 +28,19 @@ module.exports.createPages = async ({graphql,actions})=>{
         })
     }); 
 }
+
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+    if (stage === "build-html") {
+      actions.setWebpackConfig({
+        module: {
+          rules: [
+            {
+              test: /bad-module/,
+              use: loaders.null(),
+            },
+          ],
+        },
+      })
+    }
+  }
