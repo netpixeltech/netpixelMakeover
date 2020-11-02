157cf328c2d09481218e355c0aec21b3974c154f
prince <princetechspacer@gmail.com>
2020-11-02 11:56:08 +0530

    removed container

--- src/pages/booknow.js
+++ src/pages/booknow.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import ContactForm from '../components/ContactForm'
+import Footer from '../components/Footer'
+import Head from '../components/head'
 import Layout from "../components/Layout"
+import Navbar from '../components/Navbar'
 function booknow() {
     return (
-        <Layout>
-
+          <div>
+            <Navbar />
+            <Head />
             <ContactForm />
-
-        </Layout>
+            <Footer />
+          </div>
+        
     )
 }
 
